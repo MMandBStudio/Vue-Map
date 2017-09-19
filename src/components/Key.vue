eace2786ddd495733fc7ac086ce89bcc86acc016
Kev Wilson <kev.wilson84@gmail.com>
2017-09-19 13:41:52 +0100

    Remove Mac fix

--- src/components/Key.vue
+++ src/components/Key.vue
@@ -95,15 +95,6 @@
       height: 100%;
 
       .scrolling {
-        ::-webkit-scrollbar {
-          -webkit-appearance: none;
-          width: 7px;
-        }
-        ::-webkit-scrollbar-thumb {
-          border-radius: 4px;
-          background-color: rgba(0, 0, 0, .5);
-          -webkit-box-shadow: 0 0 1px rgba(255, 255, 255, .5);
-        }
         height: 100%;
         margin-bottom: 10px;
         overflow-y: scroll;
